638cb5f566f93de83d73f702d9aef98f048dcf32
Robson Coura <robson_coura@yahoo.com.br>
2023-06-22 00:07:02 -0300

    Criacao de pacote Models, com Class Filme inserindo atributos titulo, genero, duracao e criando a Class FilmeController com endpoint para adicionar filme

---
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+
+    private static List<Filme> filmes = new List<Filme>();
+
+    //Método para adicionar filme
+    [HttpPost]
+    public void AdicionarFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+}
